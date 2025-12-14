6ab162483d7b9e773a30d5d4e045d8b7e996703b
Valentin de Grammont <valentin.degrammont@gmail.com>
2025-12-14 12:57:48 +0100

    deployment test push

--- server/src/server.js
+++ server/src/server.js
@@ -17,7 +17,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'http://localhost:5173' || 'https://votre-frontend-de-production.com',
   credentials: true
 }));
 app.use(express.json());
